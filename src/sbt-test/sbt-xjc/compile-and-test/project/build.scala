cdd04a34d00b6d7be628f2c8d53ec040dde5d754
Ben McCann <ben@connectifier.com>
2015-03-15 21:01:09 -0700

    Remove unused imports

--- src/sbt-test/sbt-xjc/compile-and-test/project/build.scala
+++ src/sbt-test/sbt-xjc/compile-and-test/project/build.scala
@@ -1,6 +1,5 @@
 import com.github.retronym.sbtxjc.SbtXjcPlugin
 import sbt._
-import Keys.libraryDependencies
 
 object build extends Build {
   lazy val root = Project("main", file("."), settings = Defaults.defaultSettings ++ SbtXjcPlugin.xjcSettings ++ Seq(
